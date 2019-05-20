94cce60c1189c5c7e9f8b24bac39656bd8fa9350
alternative_backend <alternative_backend@wp.pl>
2019-05-21 01:41:25 +0200

    adding static dir in settings

--- alternative_backend/alternative_backend/settings.py
+++ alternative_backend/alternative_backend/settings.py
@@ -126,4 +126,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 BARCODE_LENGHT = 12
+
+
